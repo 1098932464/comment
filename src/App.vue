700780914caa788f685484a8af91b77556210aa4
gesh1le1 <1098932464@qq.com>
2022-10-30 02:02:31 +0800

    ddd 	modified:   package.json 	modified:   src/App.vue 	deleted:    yarn.lock

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,54 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="div" contenteditable="true" @input="oninput" class="comment">
+      {{value}}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      value:'啊啊啊啊啊啊啊啊'
+    }
+  },
+  mounted(){
+  let selection =  window.getSelection()
+    var range = document.createRange();
+    console.log(document.getElementById('div'));
+    range.selectNodeContents(document.getElementById('div'));
+       selection.addRange(range)
+       selection.collapseToEnd()
+  },
+  methods:{
+    oninput(e){
+      // 移动光标到末尾
+    let selection =  window.getSelection()
+    var range = document.createRange();
+    range.selectNodeContents(e.target);
+// console.log(e.target);
+let text = e.target.innerText
+console.log(text);
+// range.collapse(false);
+
+// range.setStart(e.target, 0);
+// range.setEnd(e.target, text.length-2);
+selection.removeAllRanges();
+    selection.addRange(range)
+    console.log(selection);
+    // selection.collapseToEnd()
+
+    }
   }
 }
 </script>
 
-<style>
+<style >
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +56,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
 }
+  .comment{
+    border: 1px solid #ddd;
+  }
 </style>
